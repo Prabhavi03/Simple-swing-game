f990b1ca1fa063bae1d627a34c007d272430335a
Prabhavi Mishra <125802799+Prabhavi03@users.noreply.github.com>
2023-02-19 17:53:52 +0530

    Add files via upload

---
+++ snakegame/src/frame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class frame extends JFrame {
+    frame() {
+        this.add(new panel());
+        this.setTitle("Snakegame");
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+    }
+}
